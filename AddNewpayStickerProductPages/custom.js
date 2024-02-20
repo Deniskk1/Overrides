a627a51d19dc80c0020e2d00d0165938bd569e95
Denis <denis.kyosev@decathlon.com>
2024-02-20 17:19:28 +0200

    Modify unicredit and newpay overrides

--- AddNewpayStickerProductPages/custom.js
+++ AddNewpayStickerProductPages/custom.js
@@ -1,38 +1,38 @@
 function addNewPaySticker() {
-    var productFrontPrice = document.querySelector(
-      "#product .price-current .price-text"
-    );
+  var productFrontPrice = document.querySelector(
+    "#product .price-current .price-text"
+  );
 
-    if (productFrontPrice) {
-      var priceText = productFrontPrice.textContent.replace(/\s/g, "");
-      var productPrice = parseFloat(priceText);
+  if (productFrontPrice) {
+    var priceText = productFrontPrice.textContent.replace(/\s/g, "");
+    var productPrice = parseFloat(priceText);
 
-      if (productPrice > 20 && productPrice < 2000) {
-        var priceRow = document.querySelector("#product .product-info_reviews");
+    if (productPrice > 20 && productPrice < 2000) {
+      var priceRow = document.querySelector("#product .product-info_reviews");
 
-        if (priceRow) {
-          var newPayBanner = document.createElement("div");
-          newPayBanner.classList.add("newpay-banner");
+      if (priceRow) {
+        var newPayBanner = document.createElement("div");
+        newPayBanner.classList.add("newpay-banner");
 
-          var newPayImg = document.createElement("img");
-          newPayImg.classList.add("newpay-img");
-          newPayImg.src =
-            "https://contents.mediadecathlon.com/s992409/k$1042d7eaa8945fd401c3873c35d439be/newpay.png";
-          newPayImg.alt = "Вземи сега, плати по-късно";
+        var newPayImg = document.createElement("img");
+        newPayImg.classList.add("newpay-img");
+        newPayImg.src =
+          "https://contents.mediadecathlon.com/s992409/k$1042d7eaa8945fd401c3873c35d439be/newpay.png";
+        newPayImg.alt = "Вземи сега, плати по-късно";
 
-          var newPayText = document.createElement("p");
-          newPayText.classList.add("newpay-text");
-          newPayText.textContent = "Вземи сега, плати по-късно";
+        var newPayText = document.createElement("p");
+        newPayText.classList.add("newpay-text");
+        newPayText.textContent = "Вземи сега, плати по-късно";
 
-          newPayBanner.appendChild(newPayImg);
-          newPayBanner.appendChild(newPayText);
+        newPayBanner.appendChild(newPayImg);
+        newPayBanner.appendChild(newPayText);
 
-          priceRow.parentNode.insertBefore(newPayBanner, priceRow.nextSibling);
-        }
+        priceRow.parentNode.insertBefore(newPayBanner, priceRow.nextSibling);
       }
     }
   }
+}
 
-  setTimeout(function() {
-    addNewPaySticker();
-  }, 1000);
+setTimeout(function () {
+  addNewPaySticker();
+}, 1500);
